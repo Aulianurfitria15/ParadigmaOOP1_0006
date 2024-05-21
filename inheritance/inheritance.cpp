50b0f6180860c7034f1367a5c7d9c12861c76f77
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-05-21 21:38:17 +0700

    menambahkan class orang dan menmabahkan akses modifier public

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -19,26 +19,3 @@ public:
 	}
 };
 
-class pelajar : public orang {
-public:
-	string sekolah;
-
-	pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
-		cout << "pelajar dibuat\n" << endl;
-	}
-	~pelajar() {
-		cout << "pelajar dihapus\n" << endl;
-	}
-	string perkenalan() {
-		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
-	}
-};
-
-int main() {
-	pelajar siswa1("andi laksono", "SMAN ! BANTUL");
-	cout << siswa1.perkenalan();
-	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
-
-	return 0;
-
-}
